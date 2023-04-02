420377a34e10bc9069610af1d6e8f14e64fba50e
Ben-Corbet <bencorbet01@gmail.com>
2023-04-02 14:23:46 -0400

    Update BookstoreApp.jar

--- src/bookstoreapp/BookstoreApp.java
+++ src/bookstoreapp/BookstoreApp.java
@@ -34,175 +34,182 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
-/**
- *
- * @author ahnaf
- */
-public class BookstoreApp extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        
-        VBox grid = new VBox();
-        VBox l2 = new VBox();
-        VBox owner = new VBox();
-        
-        grid.setAlignment(Pos.CENTER);
-//        grid.setHgap(10);
-//        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        l2.setAlignment(Pos.CENTER);
-        l2.setPadding(new Insets(25, 25, 25, 25));
-        owner.setAlignment(Pos.CENTER);
-        owner.setPadding(new Insets(25, 25, 25, 25));
-        
-        Scene scene = new Scene(grid, 650, 550);
-        Scene customerScene = new Scene(l2,650 ,550);
-        Scene ownerScene = new Scene(owner,650, 550);
-        
-        Text loginTitle = new Text("Welcome to JavaReads!");
-        loginTitle.setFont(Font.font("Cambria", FontWeight.NORMAL,24));
-        Label userName = new Label("Username: ");
-        Label password = new Label("Password: ");
-         
-        TextField nameInput = new TextField();
-        nameInput.setPrefWidth(50);
-        
-        PasswordField passInput = new PasswordField();
-
-        grid.getChildren().add(loginTitle);
-        grid.getChildren().add(userName);
-        grid.getChildren().add(nameInput);
-        grid.getChildren().add(password);
-        grid.getChildren().add(passInput);
-        
-        
-        Button signIn = new Button("Sign in");
-        HBox hbSignIn = new HBox(10);
-        hbSignIn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbSignIn.getChildren().add(signIn);
-        grid.getChildren().add(hbSignIn);
-        signIn.setOnAction(e -> primaryStage.setScene(customerScene));
-        
-        Button button2 = new Button("Backwards");
-        button2.setOnAction(e -> primaryStage.setScene(scene));
-        
-        Text nullInfo = new Text();
-        grid.getChildren().add(nullInfo);
-   
-        Label customer = new Label("customer");
-        l2.getChildren().addAll(customer,button2);
-        
-        Button bookButton = new Button("Books");
-        Button cusButton = new Button("Customers");
-        Button logoutButton = new Button("Logout");
-        
-        owner.getChildren().addAll(bookButton,cusButton,logoutButton);
-        
-        signIn.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent e) {
-            if(nameInput.getText().equals("") || passInput.getText().equals("")){
-                nullInfo.setFill(Color.FIREBRICK);
-                nullInfo.setText("Please enter your information.");
-            }
-             
-            else if(nameInput.getText().equals("admin") && passInput.getText().equals("admin")){
-                System.out.println("OWNER");
-                primaryStage.setScene(ownerScene);
-            }
-            else{
-                System.out.println("CUSTOMER");
-                primaryStage.setScene(customerScene);
-            }
-        }
-        });   
-        
-        cusButton.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent e) {
-            primaryStage.setScene(customerList(scene, primaryStage));
-        }
-        });
-        primaryStage.setTitle("BookStoreApp");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    
-    public Scene customerList(Scene scene,Stage primaryStage){
-        VBox cusList = new VBox();
-        TableView table = new TableView();
-        TableColumn<Customer, String> column1= new TableColumn<>("Name");
-        table.getColumns().add(column1);
-        
-        TableColumn<Customer, String> column2= new TableColumn<>("Password");
-        table.getColumns().add(column2);
-        
-        TableColumn<Customer, String> column3= new TableColumn<>("Points");
-        table.getColumns().add(column3);
-        
-        Button add = new Button("Add");
-        Button delete = new Button("Delete");
-        Button back = new Button("Back");
-        
-        Scene customerList = new Scene(cusList,650, 550);
-        Label userName = new Label("Username: ");
-        Label password = new Label("Password: ");
-        
-        TextField nameInput = new TextField();
-        PasswordField passInput = new PasswordField();
-        
-        cusList.getChildren().addAll(table,userName,nameInput,password,passInput,add,delete,back);
-        
-        back.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent e) {
-            primaryStage.setScene(scene);
-        }
-        
-        });   
-        
-        add.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent e) {
-            Owner owner = new Owner();
-            owner.addNewCustomer();
-        }
-        
-        });
-        return customerList;
-    }
-    
-     public static void SaveCustomers(String thing){         //This function adds the books created to the Books.txt file
-        
-        FileOutputStream fos = null;
-
-        try {
-            fos = new FileOutputStream("Customers.txt", true);          //Open the file
-            
-            byte[] s_array = thing.getBytes();                      //Turn the string into bytes
-            fos.write(s_array);                                     //Write the bytes
-
-        } catch (FileNotFoundException ex) {                        //Throws file not found exception
-            System.out.println("Customers not found");
-        } catch (IOException ex) {
-            System.out.println("IOException on SaveBooks");         //Throws IO exception
-        }
-        finally{
-            try {
-                fos.close();                                        //Close the file
-            } catch (IOException ex) {                              //Throws couldn't close file exception
-                System.out.println("Customers not closed");
-
-            }
-        }
-    }
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-}
+import javafx.collections.ObservableList;
+//public class BookstoreApp extends Application {
+//    
+//    private final Owner owner = new Owner();
+//    
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        
+//        VBox grid = new VBox();
+//        VBox l2 = new VBox();
+//        VBox owner = new VBox();
+//        
+//        grid.setAlignment(Pos.CENTER);
+////        grid.setHgap(10);
+////        grid.setVgap(10);
+//        grid.setPadding(new Insets(25, 25, 25, 25));
+//        l2.setAlignment(Pos.CENTER);
+//        l2.setPadding(new Insets(25, 25, 25, 25));
+//        owner.setAlignment(Pos.CENTER);
+//        owner.setPadding(new Insets(25, 25, 25, 25));
+//        
+//        Scene scene = new Scene(grid, 650, 550);
+//        Scene customerScene = new Scene(l2,650 ,550);
+//        Scene ownerScene = new Scene(owner,650, 550);
+//        
+//        Text loginTitle = new Text("Welcome to JavaReads!");
+//        loginTitle.setFont(Font.font("Cambria", FontWeight.NORMAL,24));
+//        Label userName = new Label("Username: ");
+//        Label password = new Label("Password: ");
+//         
+//        TextField nameInput = new TextField();
+//        nameInput.setPrefWidth(50);
+//        
+//        PasswordField passInput = new PasswordField();
+//
+//        grid.getChildren().add(loginTitle);
+//        grid.getChildren().add(userName);
+//        grid.getChildren().add(nameInput);
+//        grid.getChildren().add(password);
+//        grid.getChildren().add(passInput);
+//        
+//        
+//        Button signIn = new Button("Sign in");
+//        HBox hbSignIn = new HBox(10);
+//        hbSignIn.setAlignment(Pos.BOTTOM_RIGHT);
+//        hbSignIn.getChildren().add(signIn);
+//        grid.getChildren().add(hbSignIn);
+//        signIn.setOnAction(e -> primaryStage.setScene(customerScene));
+//        
+//        Button button2 = new Button("Backwards");
+//        button2.setOnAction(e -> primaryStage.setScene(scene));
+//        
+//        Text nullInfo = new Text();
+//        grid.getChildren().add(nullInfo);
+//   
+//        Label customer = new Label("customer");
+//        l2.getChildren().addAll(customer,button2);
+//        
+//        Button bookButton = new Button("Books");
+//        Button cusButton = new Button("Customers");
+//        Button logoutButton = new Button("Logout");
+//        
+//        owner.getChildren().addAll(bookButton,cusButton,logoutButton);
+//        
+//        signIn.setOnAction(new EventHandler<ActionEvent>() {
+//        @Override
+//        public void handle(ActionEvent e) {
+//            if(nameInput.getText().equals("") || passInput.getText().equals("")){
+//                nullInfo.setFill(Color.FIREBRICK);
+//                nullInfo.setText("Please enter your information.");
+//            }
+//             
+//            else if(nameInput.getText().equals("admin") && passInput.getText().equals("admin")){
+//                System.out.println("OWNER");
+//                primaryStage.setScene(ownerScene);
+//            }
+//            else{
+//                System.out.println("CUSTOMER");
+//                primaryStage.setScene(customerScene);
+//            }
+//        }
+//        });   
+//        
+//        cusButton.setOnAction(new EventHandler<ActionEvent>() {
+//        @Override
+//        public void handle(ActionEvent e) {
+//            primaryStage.setScene(customerList(scene, primaryStage));
+//            primaryStage.show();
+//        }
+//        });
+//        primaryStage.setTitle("BookStoreApp");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+//    
+//    public Scene customerList(Scene scene,Stage primaryStage){
+//        VBox cusList = new VBox();
+//        TableView table = new TableView();
+//        TableColumn<Customer, String> column1= new TableColumn<>("Name");
+//        table.getColumns().add(column1);
+//        
+//        TableColumn<Customer, String> column2= new TableColumn<>("Password");
+//        table.getColumns().add(column2);
+//        
+//        TableColumn<Customer, String> column3= new TableColumn<>("Points");
+//        table.getColumns().add(column3);
+//        table.setItems(customers);
+//        Button add = new Button("Add");
+//        Button delete = new Button("Delete");
+//        Button back = new Button("Back");
+//        
+//        
+//        Label userName = new Label("Username: ");
+//        Label password = new Label("Password: ");
+//        
+//        TextField nameInput = new TextField();
+//        PasswordField passInput = new PasswordField();
+//        
+//       
+//        
+//        back.setOnAction(new EventHandler<ActionEvent>() {
+//        @Override
+//        public void handle(ActionEvent e) {
+//            primaryStage.setScene(scene);
+//        }
+//        
+//        });   
+//        
+//        add.setOnAction((ActionEvent e) -> {
+//            customers.add(new Customer(nameInput.getText(),passInput.getText(),0));
+//         });
+// 
+//        
+//        cusList.getChildren().addAll(table,userName,nameInput,password,passInput,add,delete,back);
+//        Scene customerList = new Scene(cusList,650, 550);
+//        return customerList;
+//    }
+//    
+//    
+//    ObservableList<Customer> customers = FXCollections.observableArrayList();
+////    public ObservableList<Customer> addCustomers(){
+////        customers.addAll(owner.getCustomers());
+////        return customers;
+////    }
+//    
+//    
+//     public static void SaveCustomers(String thing){         //This function adds the books created to the Books.txt file
+//        
+//        FileOutputStream fos = null;
+//
+//        try {
+//            fos = new FileOutputStream("Customers.txt", true);          //Open the file
+//            
+//            byte[] s_array = thing.getBytes();                      //Turn the string into bytes
+//            fos.write(s_array);                                     //Write the bytes
+//
+//        } catch (FileNotFoundException ex) {                        //Throws file not found exception
+//            System.out.println("Customers not found");
+//        } catch (IOException ex) {
+//            System.out.println("IOException on SaveBooks");         //Throws IO exception
+//        }
+//        finally{
+//            try {
+//                fos.close();                                        //Close the file
+//            } catch (IOException ex) {                              //Throws couldn't close file exception
+//                System.out.println("Customers not closed");
+//
+//            }
+//        }
+//    }
+//    
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//    
+//}
