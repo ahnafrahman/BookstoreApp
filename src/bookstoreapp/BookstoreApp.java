38f46fd0598f9e6b03b574b850160a003499a269
Mudassir-Ashraf <129450079+Mudassir-Ashraf@users.noreply.github.com>
2023-04-02 15:08:45 -0400

    changed lines 472-573

--- src/bookstoreapp/BookstoreApp.java
+++ src/bookstoreapp/BookstoreApp.java
@@ -477,7 +477,7 @@ public class Main extends Application {
         int pointsEarned, i = 0, bookCount = 0;
         String[][] booksBought = new String[25][2];
 
-        for(Book b: Owner.books){
+        for(Book b: Owner.bookList){
             if(b.getSelect().isSelected()){
                 subtotal += b.getPrice();
                 booksBought[i][0] = b.getTitle();
@@ -570,7 +570,7 @@ public class Main extends Application {
         screen.getChildren().addAll(header, receipt, bottom);
 
         cos.getChildren().addAll(screen);
-        Owner.books.removeIf(b -> b.getSelect().isSelected());
+        Owner.bookList.removeIf(b -> b.getSelect().isSelected());
         return cos;
     }//complete
 
