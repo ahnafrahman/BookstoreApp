a066a070fd0252008f21f14d112ff0324267c202
Christian Sorto <christiansrto@gmail.com>
2023-04-01 22:21:16 -0400

    Working customer login Screen
    Co-Authored-By: Ahnaf Rahman <90946201+ahnafrahman@users.noreply.github.com>

--- src/bookstoreapp/BookstoreApp.java
+++ src/bookstoreapp/BookstoreApp.java
@@ -6,6 +6,7 @@
 package bookstoreapp;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -18,6 +19,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import static javafx.scene.paint.Color.color;
 import javafx.scene.text.FontWeight;
@@ -34,17 +36,18 @@ public class BookstoreApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         
-        GridPane grid = new GridPane();
+        VBox grid = new VBox();
+        VBox l2 = new VBox();
         grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
+//        grid.setHgap(10);
+//        grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-            
-        Scene scene = new Scene(grid, 300, 250);
+        l2.setAlignment(Pos.CENTER);
+        l2.setPadding(new Insets(25, 25, 25, 25));
         
-        primaryStage.setTitle("BookStoreApp");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(grid, 300, 250);
+        Scene customerScene = new Scene(l2,300,250);
+        
         
         Text loginTitle = new Text("Welcome to JavaReads!");
         loginTitle.setFont(Font.font("Cambria", FontWeight.NORMAL,24));
@@ -54,56 +57,67 @@ public class BookstoreApp extends Application {
         TextField nameInput = new TextField();
         PasswordField passInput = new PasswordField();
         
-        grid.add(loginTitle, 0,0,2,1);
-        grid.add(userName,0,1);
-        grid.add(nameInput,1,1);
-        grid.add(password,0,2);
-        grid.add(passInput,1,2);
+        grid.getChildren().add(loginTitle);
+        grid.getChildren().add(userName);
+        grid.getChildren().add(nameInput);
+        grid.getChildren().add(password);
+        grid.getChildren().add(passInput);
         
         
         Button signIn = new Button("Sign in");
         HBox hbSignIn = new HBox(10);
         hbSignIn.setAlignment(Pos.BOTTOM_RIGHT);
         hbSignIn.getChildren().add(signIn);
-        grid.add(hbSignIn, 1, 4);
+        grid.getChildren().add(hbSignIn);
+        signIn.setOnAction(e -> primaryStage.setScene(customerScene));
+        
+        Button button2 = new Button("Backwards");
+        button2.setOnAction(e -> primaryStage.setScene(scene));
         
         Text nullInfo = new Text();
-        grid.add(nullInfo,1,6);
+        grid.getChildren().add(nullInfo);
         
-//        signIn.setOnAction(new EventHandler<ActionEvent>() {
+        
+         
+        
+        
+        Label customer = new Label("customer");
+        l2.getChildren().addAll(customer,button2);
+        
+        signIn.setOnAction(new EventHandler<ActionEvent>() {
             
             
-//        @Override
-//        public void handle(ActionEvent e) {
+        @Override
+        public void handle(ActionEvent e) {
             if(nameInput.getText().equals("") || passInput.getText().equals("")){
                 nullInfo.setFill(Color.FIREBRICK);
                 nullInfo.setText("Please enter your information.");
             }
-                
+             
             else if(nameInput.getText().equals("admin") && passInput.getText().equals("admin"))
                 System.out.println("OWNER");
             else{
                 System.out.println("CUSTOMER");
-                signIn.setOnAction(e ->primaryStage.setScene(customerScreen()));
-               
+                primaryStage.setScene(customerScene);
             }
-//        }
-//        });
+        }
+        });   
         
-        
+        primaryStage.setTitle("BookStoreApp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
-    public Scene customerScreen(){
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        Text customer = new Text("customer");
-        grid.getChildren().add(customer);
-        Scene customerScene = new Scene(grid,600,800);
-        return customerScene; 
-    }
+//    public Scene customerScreen(){
+//        VBox l2 = new VBox();
+//        l2.setAlignment(Pos.CENTER);
+//        
+//        Text customer = new Text("customer");
+//        
+//        Scene customerScene = new Scene(l2,600,800);
+//        System.out.println("works");
+//        return customerScene; 
+//    }
     
     
     
